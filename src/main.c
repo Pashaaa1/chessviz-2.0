297d19cd024460a2db90625cde32e4c7aa1e6cc1
Илья <Grinch@localhost.localdomain>
2018-04-23 19:49:52 +0700

    Merge branch 'chess_board'into branch 'master'

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main()
     int i = 0;
     while ( i == 0) {
         char q;
-        printf("\t(s) Start or (q) Quit\n");
+        printf("\t(s) Start  or (q) Quit\n");
         printf("\t\t Command : ");
         scanf("%c", &q);
         print_new_board();
